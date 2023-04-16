5830b95bb2b1c59c8906e4fcbb5035409f8099cd
Cees-Jan Kiewiet <ceesjank@gmail.com>
2023-04-16 17:54:24 +0200

    Run all tests in a fiber
    Since all tests are executed inside a fiber, there is a default timeout of `30` seconds. To lower or raise that timeout
    this package comes with a `TimeOut` attribute. It can be set on the class and method level. When set on both the method level it takes priority over the class level.
    
    ```php
    <?php
    
    declare(strict_types=1);
    
    namespace WyriHaximus\Tests\AsyncTestUtilities;
    
    use React\EventLoop\Loop;
    use WyriHaximus\AsyncTestUtilities\AsyncTestCase;
    use WyriHaximus\AsyncTestUtilities\TimeOut;
    
    use function React\Async\async;
    use function React\Async\await;
    use function React\Promise\resolve;
    use function React\Promise\Timer\sleep;
    use function time;
    
    #[TimeOut(0.3)]
    final class AsyncTestCaseTest extends AsyncTestCase
    {
        #[TimeOut(1)]
        public function testAllTestsAreRanInAFiber(): void
        {
            self::expectOutputString('ab');
    
            Loop::futureTick(async(static function (): void {
                echo 'a';
            }));
    
            await(sleep(1));
    
            echo 'b';
        }
    
        public function testExpectCallableExactly(): void
        {
            $callable = $this->expectCallableExactly(3);
    
            Loop::futureTick($callable);
            Loop::futureTick($callable);
            Loop::futureTick($callable);
        }
    
        public function testExpectCallableOnce(): void
        {
            Loop::futureTick($this->expectCallableOnce());
        }
    }
    ```

--- tests/AsyncTestCaseTest.php
+++ tests/AsyncTestCaseTest.php
@@ -6,12 +6,31 @@ namespace WyriHaximus\Tests\AsyncTestUtilities;
 
 use React\EventLoop\Loop;
 use WyriHaximus\AsyncTestUtilities\AsyncTestCase;
+use WyriHaximus\AsyncTestUtilities\TimeOut;
 
+use function React\Async\async;
+use function React\Async\await;
 use function React\Promise\resolve;
+use function React\Promise\Timer\sleep;
 use function time;
 
+#[TimeOut(1)]
 final class AsyncTestCaseTest extends AsyncTestCase
 {
+    #[TimeOut(0.1)]
+    public function testAllTestsAreRanInAFiber(): void
+    {
+        self::expectOutputString('ab');
+
+        Loop::futureTick(async(static function (): void {
+            echo 'a';
+        }));
+
+        await(sleep(0.01));
+
+        echo 'b';
+    }
+
     public function testAwait(): void
     {
         $value = time();
